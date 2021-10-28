76756a6775779193821cc3958d7af417286aaa81
kwonsean <gownn12@naver.com>
2021-10-28 10:35:38 +0900

    feat: Add some options
    - 참가자 숫자 입력 유도
    - 재도전 선택 기능
    - 엔터 입력 가능
    
    - ㄱ,ㄴ,ㄷ같은 입력 방어
    - 지금까지 입력된 단어 list로 보여주기 및 중복단어 입력 체크

--- section3/main.js
+++ section3/main.js
@@ -1,4 +1,11 @@
-const totalGamers = Number(prompt('총 참가자의 수를 입력해 주세요.'))
+let totalGamers = Number(prompt('총 참가자의 수를 입력해 주세요.'))
+
+// 숫자가 아닌값 입력시 숫자 입력 유도
+while(!totalGamers){
+  if(!totalGamers){
+    totalGamers = Number(prompt('숫자를 입력해주세요!!!'))
+  }
+}
 
 const gamerNumber = document.querySelector('#order')
 const inputEl = document.querySelector('input')
@@ -6,35 +13,43 @@ const buttonEl = document.querySelector('button')
 const wordEl = document.querySelector('#word')
 let word = ''
 let newWord = ''
-console.log(gamerNumber)
 
 function onClickButton(){
-  if(!word){
+  if(!word || word[word.length -1] === newWord[0]){
     word = newWord
     wordEl.textContent = word
+    const gameTurn = Number(gamerNumber.textContent)
+    gameTurn + 1  <= totalGamers ? gamerNumber.textContent = gameTurn + 1 : gamerNumber.textContent = 1
     inputEl.value = ''
     inputEl.focus()
   } else{
-    if(word[word.length -1] === newWord[0]){
-      word = newWord
-      wordEl.textContent = word
-      const gameTurn = Number(gamerNumber.textContent)
-      gameTurn + 1  <= totalGamers ? gamerNumber.textContent = gameTurn + 1 : gamerNumber.textContent = 1
+    alert(`'${word[word.length-1]}'로 시작하는 단어여야 합니다!`)
+    
+    // 재도전 선택 구현 
+    let retry = confirm('재도전 하시겠습니까?')
+    if(retry){
       inputEl.value = ''
       inputEl.focus()
-
     } else{
-      alert('틀렸습니다.')
-      inputEl.value = ''
-      inputEl.focus()
+      window.location.reload()
     }
   }
 }
 
 function onInput(event){
   newWord = event.target.value
+  if(newWord === ' ' || newWord[newWord.length -1] === ' '){
+    alert('공백을 입력하면 안됩니다.')
+    inputEl.value = ''
+    inputEl.focus()
+  }
   console.log(newWord)
 }
 
 buttonEl.addEventListener('click', onClickButton)
 inputEl.addEventListener('input', onInput)
+
+// 엔터를 눌러도 입력이 되도록 구현
+inputEl.addEventListener('keyup', (event)=>{
+  if(event.key === 'Enter') onClickButton()
+})
