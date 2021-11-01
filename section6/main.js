c57441a0e5aa757e6aec33dbf30481a2c0c904e4
kwonsean <gownn12@naver.com>
2021-11-01 14:20:00 +0900

    docs: Oranized the code
    - 코드를 정리하였습니다.
    - style부분에 line-height를 추가하여 요소가 추가될때 움직이지 않도록
      하였습니다.

--- section6/main.js
+++ section6/main.js
@@ -8,17 +8,16 @@ while (candidate.length > 0){
   const value = spliceArray[0]
   shuffle.push(value)
 }
-console.log(shuffle)
 
 const winBalls = shuffle.slice(0, 6).sort((a,b) => a-b) // slice는 원본 영향 x 
 const bonus = shuffle[6] 
-console.log(winBalls, bonus)
+console.log('winBalls:', winBalls, ' bonus:',bonus)
 
-const $result = document.querySelector('#result')
 function drawBall(number, $parent){
     const $ball = document.createElement('div')
     $ball.className = 'ball'
     $ball.textContent = number
+    // mission 부분 
     if(number < 10){
       $ball.classList.add('red')
     } else if(number < 20){
@@ -32,6 +31,8 @@ function drawBall(number, $parent){
     }
     $parent.appendChild($ball)
 }
+
+const $result = document.querySelector('#result')
 for(let i =0; i<winBalls.length; i++){
   setTimeout(()=>{
     drawBall(winBalls[i], $result)
